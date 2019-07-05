09a2bfa04ed68a43aa2695e4dd2cd63f9856d401
Keels <keely.retzloff@gmail.com>
2019-07-05 14:11:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
+
+
+class App extends Component {
+ constructor(props) {
+ 	super(props);
+ 	this.state = {
+ 		businesses: []
+ 	};
+ 	this.searchYelp = this.searchYelp.bind(this);
+ }
+
+searchYelp(term, location, sortBy) {
+	Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+		this.setState({businesses: businesses});
+	});
+}
+
+render() {
+    return (
+    <div className="App">
+ 		<h1>ravenous</h1>
+ 		<SearchBar searchYelp={this.searchYelp} />
+ 		<BusinessList businesses={this.state.businesses} />
+	</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = "cOrj4ogNMTiH2Hs1-8dhzAwcY5leBLLISqlR1UJUu4owroPRgacOXdxbEcenrHXa6xb38pr4xF7yIeUhOaNHMUZJu3_eKy9Wxu0xnfCW7YdSIzbgBncmu0ocl2-dXHYx";
+
+const Yelp = {
+	searchYelp(term, location, sortBy) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+		{ 
+ 			headers: {
+   				Authorization: `Bearer ${apiKey}` 
+   				}
+ 		}).then(response => {
+ 			return response.json();
+ 		}).then((jsonResponse) => {
+ 			if(jsonResponse.businesses) {
+ 				return jsonResponse.businesses.map(business =>{
+ 					return {id: business.id,
+ 						imageSrc: business.image_url,
+ 						name: business.name,
+ 						address: business.location.address1,
+ 						city: business.location.city,
+ 						state: business.location.state,
+ 						zipCode: business.location.zip_code,
+ 						category: business.categories[0].title,
+ 						rating:business.rating,
+ 						reviewCount: business.review_count,
+ 					}
+ 				});
+ 			}
+ 		});
+	}
+};
+
+export default Yelp;
